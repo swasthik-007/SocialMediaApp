901eb623890a1e40809b4847e043131424630199
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-11 11:17:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+import Navbar from './Navbar'
+function App() {
+
+
+  return (
+    <>
+    <Navbar />
+     {/* Welcome */}
+     {/* <Button variant="contained">Hello</Button> */}
+    </>
+  )
+}
+
+export default App
