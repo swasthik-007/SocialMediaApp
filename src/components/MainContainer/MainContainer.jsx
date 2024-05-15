b6915d83508e68bae29c409dcfe499aa334d3da7
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-15 17:05:21 +0530

    added the liked button

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
-
 import Grid from '@mui/material/Unstable_Grid2';
-
 import Box from '@mui/material/Box';
-
 import PostCardList from '../PostCardlist/PostcardList';
 import UserList from '../UserList/UserList'
 
 export default function RowAndColumnSpacing() {
   return (
-    <Box display={'flex'} justifyContent={'space-around'}  sx={{ width: '100%',mt:'3rem' }}>
-      <Grid container alignItems={'start'} justifyContent={'center'} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid  md={6}>
+    <Box container  sx={{mt:'3rem' }}>
+      <Grid container alignItems={'start'} justifyContent={'center'} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
+        <Grid>
           {/* {users } */}
          <UserList />
         </Grid>
-        <Grid container alignItems={'center'} justifyContent={'center'} direction={'column'}   md={6}>
+        <Grid 
+        container
+        alignItems={'center'} 
+        justifyContent={'center'} 
+        direction={'column'}   
+        md={8}>
           {/* {Posts} */}
        <PostCardList/>
        
--- src/components/postCard.jsx
+++ src/components/postCard.jsx
@@ -11,17 +11,15 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useState } from 'react';
 
 
 export default function PostCard({authorfirstname,image,content}) {
 
-
+const  [isLiked,setIsLiked] =useState(false);
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345 ,mb:'3rem' }}>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -46,7 +44,11 @@ export default function PostCard({authorfirstname,image,content}) {
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+          {
+            (isLiked) ? <FavoriteIcon onClick={ ()=> setIsLiked(!isLiked)} /> : <FavoriteBorderIcon onClick={ ()=> setIsLiked(!isLiked)} />
+          }
+          
+          
         </IconButton>
        
       </CardActions>
